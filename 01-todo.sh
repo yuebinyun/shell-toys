0be480017999a12f193f59610d301154a28a94a3
yuebinyun <yuebinyun@gmail.com>
2020-01-07 00:08:38 +0800

    update

--- 01-todo.sh
+++ 01-todo.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-echo [`pwd`]
+dir=`ls`
 version="## "*"."*"."*
-while read line
+todo="update.md"
+for i in $dir
 do
-  if [[ $line == $version ]]; then
-    break
-  else
-    echo "$line"
+  if [[ -d $i ]]; then
+    cd $i
+    echo "==> [$i]"
+    if [[ -f $todo ]]; then
+      while read line
+      do
+        if [[ $line == $version ]]; then
+	  break
+	else
+          echo "$line"
+	fi
+      done < $todo
+    else
+      echo "[ERROR] 该项目下没有 update.md 文件"
+      echo ""
+    fi
+    cd ..
   fi
-done < update.md
+done
